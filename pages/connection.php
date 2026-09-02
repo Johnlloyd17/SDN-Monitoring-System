6d378fef84e474a18ad1bee505cd39f0b3605bbe
John Lloyd Caban <90074153+Johnlloyd17@users.noreply.github.com>
2026-09-02 17:56:46 +0800

    New Updated

--- pages/connection.php
+++ pages/connection.php
@@ -1,7 +1,8 @@
 <?php
 /** @var mysqli $con Database connection */
 global $con;
-$con = mysqli_connect('localhost','root','','dict_proj', 4306) or die(mysqli_connect_error());
+$port = getenv('DB_PORT') ?: 3306;
+$con = mysqli_connect('localhost', 'root', '', 'dict_proj', (int)$port) or die(mysqli_connect_error());
 
 date_default_timezone_set("Asia/Manila");
 ?>
